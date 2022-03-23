8348fc4b6126241ddef170af9266de55e21d38d5
Michael O'Reilly <86013664+MichaelOrSd@users.noreply.github.com>
2022-03-23 10:16:35 -0230

    init

--- QAP3/index.js
+++ QAP3/index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const app = express();
-const session = require('express-session');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-// 	session({
-// 		secret: 'cape broyle',
-// 		resave: false,
-// 		saveUninitialized: true,
-// 	})
-// );
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}.`);
@@ -22,15 +14,9 @@ app.set('view engine', 'ejs');
 
 const homeRouter = require('./routes/home');
 const storesRouter = require('./routes/stores');
-// const languagesRouter = require('./routes/languages');
-// const aboutRouter = require('./routes/about');
-// const rentalFilms = require('./routes/rentalFilms');
 
 app.use('/', homeRouter);
 app.use('/stores', storesRouter);
-// app.use('/languages', languagesRouter);
-// app.use('/about', aboutRouter);
-// app.use('/rentalFilms', rentalFilms);
 
 app.use((req, res) => {
 	res.status(404).render('404');
--- QAP3/routes/stores.js
+++ QAP3/routes/stores.js
@@ -23,19 +23,13 @@ router.get('/:id', async (req, res) => {
 
 	if (req.query.rentalFilms) {
 		var revenue = await storesDal.getFilmRevenueByStoreIdFilm(req.params.id, req.query.rentalFilms);
-		// var revenue = [{title: 'Fat Cat', monies: 78.90},
-		//    {title: 'Greatest', monies: 114.45},
-		//    {title: 'Testing is good', monies: 67.80}];
 	} else {
 		var revenue = await storesDal.getFilmRevenueByStoreId(req.params.id);
-		// var revenue = [{name:'French', title: 'Spiderman', monies: 780.90},
-		//     {name:'English', title: 'Greatest Wonder', monies: 234.45},
-		//     {name:'German', title: 'Good Enough', monies: 125.80}];
 	}
 
 	if (store.length === 0) res.render('norecord');
 	else {
-		res.render('storedetails.ejs', { store, revenue, rentalFilms /*rentalFilms: req.query.rentalFilms*/ });
+		res.render('storedetails.ejs', { store, revenue, rentalFilms });
 	}
 });
 
